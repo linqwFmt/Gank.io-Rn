2dafd0723063d0740378941bb24d1e21ab8d7f2d
lqw <1105896230@qq.com>
2017-03-13 09:28:20 +0800

    修改界面

--- app/commom/home/Content.js
+++ app/commom/home/Content.js
@@ -11,12 +11,12 @@ import {
     StyleSheet,
     Text
 }from 'react-native'
-import defalutColor from '../Constants'
+import Common from '../Constants'
 export default class ContentItem extends Component {
 
     render() {
         let datas = this.props.datas;
-        let titleStyle=this._titleStyle();
+        let titleStyle = this._titleStyle();
         return (
             <View style={styles.container}>
                 <Text style={[styles.title,titleStyle]}>{datas[0].type}</Text>
@@ -26,22 +26,23 @@ export default class ContentItem extends Component {
         )
     }
 
-    _titleStyle(){
+    _titleStyle() {
         let datas = this.props.datas;
-        if(datas[0].type=='Android')
+        if (datas[0].type == 'Android')
             return styles.androidTitle
-        else if (datas[0].type=='iOS')
+        else if (datas[0].type == 'iOS')
             return styles.iOSTitle
-        else if (datas[0].type=='休息视频')
+        else if (datas[0].type == '休息视频')
             return styles.restTitle
-        else if (datas[0].type=='拓展资源')
+        else if (datas[0].type == '拓展资源')
             return styles.extentTitle
-        else if (datas[0].type=='前端')
+        else if (datas[0].type == '前端')
             return styles.htmlTitle
     }
 
     _listItem() {
         let mViews = [];
+        console.log(this.props.datas.length)
         for (let i = 0; i < this.props.datas.length; i++)
             mViews.push(this._article(this.props.datas[i].desc, this.props.datas[i].who))
         return (
@@ -53,7 +54,7 @@ export default class ContentItem extends Component {
         return (
             <View style={styles.articleContainer}>
                 <Text style={styles.desc}>{desc}</Text>
-                <Text style={styles.article}>   via. {article}</Text>
+                <Text style={styles.article}> via. {article}</Text>
             </View>
         )
     }
@@ -73,30 +74,30 @@ var styles = StyleSheet.create({
         padding: 10,
         fontSize: 18
     },
-    androidTitle:{
-        color:defalutColor.android
+    androidTitle: {
+        color: Common.defaultColor.android
     },
-    iOSTitle:{
-        color:defalutColor.iOS
+    iOSTitle: {
+        color: Common.defaultColor.iOS
     },
-    restTitle:{
-    color:defalutColor.reset
-   },
-    extentTitle:{
-        color:defalutColor.extend
+    restTitle: {
+        color: Common.defaultColor.reset
     },
-    htmlTitle:{
-        color:defalutColor.html
+    extentTitle: {
+        color: Common.defaultColor.extend
+    },
+    htmlTitle: {
+        color: Common.defaultColor.html
     },
     desc: {
-         color:'#161615'
+        color: '#161615'
     },
-    article:{
-         color:'#c0c0bd'
+    article: {
+        color: '#c0c0bd'
     },
-    articleContainer:{
-        flex:1,
-        flexDirection:'row',
-        margin:10
+    articleContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 10
     }
 })
